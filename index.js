80af347e5d63f4766ddc584159c7b002eb01111a
XxXProgramadoRXxX <leandrojunio77@gmail.com>
2023-09-10 12:25:20 -0300

    PORTA ATUALIZADA

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const lugaresController = require("./lugares/lugaresController")
 app.use(bodyParaser.urlencoded({extends: false}))
 app.use(bodyParaser.json())
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 7000
 
 app.use("/", lugaresController)
 app.get("/", (req, res) => {
